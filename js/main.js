d0eda6eea0db12b7125b5b67b61cdc2de2130b5b
morfiy <keks46@yandex.ru>
2025-04-02 16:41:16 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -14,3 +14,13 @@ $(document).ready(function () {
     });
 });
 
+let limit = 2;
+var checks = document.querySelectorAll(".input-checkbox");
+var max = 2;
+for (var i = 0; i < checks.length; i++)
+    checks[i].onclick = selectiveCheck;
+function selectiveCheck(event) {
+    var checkedChecks = document.querySelectorAll(".input-checkbox:checked");
+    if (checkedChecks.length >= max + 1)
+        return false;
+}
